df302b7b06d8ad042777f7a72b0c6971431d2b32
goldysehgal <52884019+goldysehgal@users.noreply.github.com>
2020-11-12 17:48:27 +0530

    initial commit

---
+++ home/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+
+class Message(models.Model):
+    sender = models.CharField(max_length=100)
+    email = models.EmailField()
+    date = models.DateField(auto_now=True)
+    view = models.TextField()
+    reply = models.TextField(default='None')
+
+    def __str__(self):
+        return self.sender
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    FILTER_CHOICES = [
+    ('artificial_intelligence', 'Artificial Intelligence'),
+    ('programming', 'Programming'),
+    ('web_development', 'Web Development'),
+    ('mobile_development', 'Mobile Development'),
+    ('data', 'Data')
+    ]
+    filter = models.CharField(max_length=23, choices=FILTER_CHOICES)
+    date = models.DateField(auto_now=True)
+    source = models.URLField()
+    image = models.ImageField(upload_to='Playlist-Images')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    username = models.CharField(max_length=100)
+    playlist = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    view = models.TextField()
+
+    def __str__(self):
+        return self.view
+
+class Feedback(models.Model):
+    playlist = models.CharField(max_length=50)
+    likedUsers = ArrayField(models.CharField(max_length=100), default=list, blank=True)
+    dislikedUsers = ArrayField(models.CharField(max_length=100), default=list, blank=True)
+
+    def __str__(self):
+        return self.playlist
